ba00b8647dace10a44ca419f10ba95f08534ee04
fcamungwa <fcamungwa@gmail.com>
2022-07-02 12:16:01 -0700

    added more tests

--- jokes/tests.py
+++ jokes/tests.py
@@ -1,8 +1,17 @@
 from django.test import TestCase
-
+from django.urls import reverse
+from rest_framework import status
+import json
 # Create your tests here.
 
 class URLTests(TestCase):
-    def test_testcornyoutput(self):
-        responsefromfxn = self.client.get("/api/jokes/corny")
+    cornylink = reverse('corny')
+    def test_testcornystatus(self):
+        responsefromfxn = self.client.get(self.cornylink)
         self.assertEqual(responsefromfxn.status_code,200)
+
+
+    def test_testcornyoutputcontent(self):
+        responsefromfxn = self.client.get(self.cornylink)
+        self.assertEqual(responsefromfxn.status_code,status.HTTP_200_OK)
+        self.assertEqual(json.loads(responsefromfxn.content),'hello')
